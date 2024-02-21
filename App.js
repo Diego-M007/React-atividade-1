baab54da55daed5eac1af9dc768eace2983ce649
Diego Miranda <125681791+Diego-M007@users.noreply.github.com>
2024-02-21 13:07:44 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ alignItems: 'flex-end', }}><Text style={{ color: 'blue', fontWeight: 'bold', fontSize: 30 }}>Diego Miranda de Oliveira</Text></View>
-      <View style={{ alignItems: 'flex-start', }}><Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, borderWidth: 1,borderColor: 'red',borderStyle: 'solid',}}>Notebook 12</Text></View>
-      <View style={{ alignItems: 'center', }}><Text style={{ color: 'black', fontSize: 20, backgroundColor: 'pink' }}>7</Text></View>
-      <View style={{ alignItems: 'center', }}><Text style={{ color: 'black', fontSize: 15, backgroundColor: 'red',borderWidth: 1,borderColor: 'black',borderStyle: 'solid' }}>Senai Nova Odessa</Text></View>
+      <View style={{ alignItems: 'flex-end', }}><Text style={{ color: 'blue', fontWeight: 'bold', fontSize: 30,padding: 5 }}>Diego Miranda de Oliveira</Text></View>
+      <View style={{ alignItems: 'flex-start', }}><Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, borderWidth: 1,borderColor: 'red',borderStyle: 'solid',padding: 5, borderRadius:50,}}>Notebook 12</Text></View>
+      <View style={{ alignItems: 'center', }}><Text style={{ color: 'black', fontSize: 20, backgroundColor: 'pink',padding: 5 }}>7</Text></View>
+      <View style={{ alignItems: 'center', }}><Text style={{ color: 'white', fontSize: 15, backgroundColor: 'gray',borderWidth: 1,borderColor: 'black',borderStyle: 'solid', padding: 5,borderRadius:50 }}>Senai Nova Odessa</Text></View>
       <StatusBar style="auto" />
     </View>
   );
