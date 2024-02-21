9a46f632c45634c76354eb333ffc676dd94048bd
Diego Miranda <125681791+Diego-M007@users.noreply.github.com>
2024-02-21 12:53:38 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={{ alignItems: 'flex-end', }}><Text style={{ color: 'blue', fontWeight: 'bold', fontSize: 30 }}>Diego Miranda de Oliveira</Text></View>
-      <View style={{ alignItems: 'flex-start', }}><Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, border: '1px solid red' }}>Notebook 12</Text></View>
+      <View style={{ alignItems: 'flex-start', }}><Text style={{ color: 'black', fontStyle: 'italic', fontSize: 25, borderWidth: 1,borderColor: 'red',borderStyle: 'solid',}}>Notebook 12</Text></View>
       <View style={{ alignItems: 'center', }}><Text style={{ color: 'black', fontSize: 20, backgroundColor: 'pink' }}>7</Text></View>
       <StatusBar style="auto" />
     </View>
